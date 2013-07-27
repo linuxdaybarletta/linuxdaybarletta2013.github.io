cb2448d942ca452409a43af22ba55772d77ff2dc
Nicola Lamacchia <nicola.lamacchia+github@gmail.com>
2013-07-27 12:58:32 +0200

    First commit

---
+++ assets/js/gtml.js
@@ -0,0 +1,6 @@
+function decode() {
+    var link = document.getElementById('mlsnd');
+    // md5 of KILLSPAM
+    link.href = link.href.replace(/377867bca0ab759b28b215d31e10ad8b/,'');
+}
+window.onload = decode;
