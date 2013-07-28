05c3d77cb1c99ce08e68195cc99f98215925f75d
Nicola Lamacchia <nicola.lamacchia+github@gmail.com>
2013-07-28 16:19:34 +0200

    Update the 404 page

--- assets/js/404.js
+++ assets/js/404.js
@@ -1,4 +1,10 @@
+// If JavaScript is disabled
+document.ready = function() {
+    $("#text").hide();
+};
+// Main animation
 window.onload = function() {
     $("#tux").delay(1000).animate({height: "55px"}, 1000);
     $("#text").delay(2000).fadeIn("slow");
+    $("#paw").delay(2050).fadeIn(800, "linear");
 };
